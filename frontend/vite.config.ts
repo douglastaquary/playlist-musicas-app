bb520fe7cc8e115587e05a8436fead15de2c2581
Douglas Taquary <douglastaquary@gmail.com>
2025-11-06 23:58:12 -0300

    Replace backend URL.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'playlist-musicas-app-production.up.railway.app',
         changeOrigin: true,
       },
     },
